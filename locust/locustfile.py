20829a92b6300659970abfcc6aaa1014cca31795
Ali <ali.bakhshi021.001@gmail.com>
2025-08-23 11:41:35 +0330

    fix: add client-headers for start username Authorization

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -3,9 +3,18 @@ from locust import HttpUser, between, task
 class QuickStartUser(HttpUser):
     wait_time = between(1, 5)
 
+    def on_start(self):
+        response = self.client.post("/accounts/api/v2/jwt/create/", data={
+            "username": "mohamad",
+            "password": "Nima4030#"
+        }).json()
+        
+        self.client.headers = {'Authorization': f"Bearer {response.get('access', None)}"}
+
     @task
     def api_list(self):
         self.client.get("/products/api/v1/post/")
+        
     @task
     def api_category(self):
         self.client.get("/products/api/v1/category/")
